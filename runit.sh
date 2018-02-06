9e2035d74fd052d7598603540659b690124bf2b4
Jan De Landtsheer <jan@gig.tech>
2018-02-06 19:27:02 +0100

    Oops forgot to revert testname for running meneja

--- runit.sh
+++ runit.sh
@@ -2,7 +2,7 @@
 
 docker run -d --rm --net=host  \
 	-v $(pwd)/911builder:/911builder -v $(pwd)/meneja:/usr/src/app \
-	--name meneja menetest:latest  \
+	--name meneja meneja:latest  \
 	0.0.0.0 8080 https://meneja.gig.tech/callback  greenitglobe.team.operations \
 	CvCJ6jm6ZZXwLwz3qWOWNsoUnN8yKEcP0KOFYZycIlJSaLhkAWal \
 	https://docs.greenitglobe.com /911builder/GIGINSTALLER.iso
